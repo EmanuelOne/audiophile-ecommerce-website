c212fcaf90ff003bd5fe64a6546f33e2e5158d68
emanuelone <emanuel4mechie@gmail.com>
2022-03-03 14:44:15 +0100

    vg

--- src/components/Storage/cookies.ts
+++ src/components/Storage/cookies.ts
@@ -1,6 +1,6 @@
 import { getCookie as GetCookie, removeCookies } from 'cookies-next'
 export const getCookie = (name: string) => {
-  return JSON.parse(GetCookie(name) || '{}')
+  return JSON.parse(GetCookie(name) ? GetCookie(name) : '{}')
 }
 export const removeCookie = (name: string) => {
   const cookie = removeCookies(name)
